4a07525349c72b3cb8b355a0ef3a5578ceb9fd43
Maria Ramos <mariaeramosmorales@gmail.com>
2020-01-12 22:51:04 -0400

    Updates

---
+++ precache-manifest.58f47d00bd11f424d64fc83af31f2746.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3abdd0ae6e7d1a520821ab79875010f1",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "ac97317b941c094f604b",
+    "url": "/portfolio/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "59800bcaa06de1b64b2f",
+    "url": "/portfolio/static/css/main.ba679ad7.chunk.css"
+  },
+  {
+    "revision": "ac97317b941c094f604b",
+    "url": "/portfolio/static/js/2.22d57d86.chunk.js"
+  },
+  {
+    "revision": "f032203ca460334c00de541c30a6078a",
+    "url": "/portfolio/static/js/2.22d57d86.chunk.js.LICENSE"
+  },
+  {
+    "revision": "59800bcaa06de1b64b2f",
+    "url": "/portfolio/static/js/main.2d8b4b62.chunk.js"
+  },
+  {
+    "revision": "6fef3673099b4dcdb973",
+    "url": "/portfolio/static/js/runtime-main.fed7eca6.js"
+  },
+  {
+    "revision": "2b983e8ae69e9c92043b983453fc11c3",
+    "url": "/portfolio/static/media/foxNgrapes.2b983e8a.mp4"
+  },
+  {
+    "revision": "af234b70adf54b1839689d9383b4ab91",
+    "url": "/portfolio/static/media/girl.af234b70.mp4"
+  },
+  {
+    "revision": "84145dba747f143327aef57092c52261",
+    "url": "/portfolio/static/media/hello.84145dba.mp4"
+  },
+  {
+    "revision": "5ce511393c1a8f15a2fe7232f2c8fd91",
+    "url": "/portfolio/static/media/parallax_preview.5ce51139.mp4"
+  },
+  {
+    "revision": "f69dedabb468ab16d2c4364a03ad993e",
+    "url": "/portfolio/static/media/skilledmatica_preview.f69dedab.mp4"
+  },
+  {
+    "revision": "de86c178f4e44419d0efae82ab2c9589",
+    "url": "/portfolio/static/media/winking_chibi.de86c178.mp4"
+  }
+]);
